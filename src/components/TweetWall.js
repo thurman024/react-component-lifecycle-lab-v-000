b0f89b7ef7441ee0cd3737383fa84bdbea75f4dd
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-06-29 13:31:33 -0500

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,29 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.newTweets.size > 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [
+        ...nextProps.newTweets,
+        ...this.state.tweets
+        ]
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
